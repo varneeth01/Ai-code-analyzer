fd29e3698533e5573e32de38d773744dccc002b9
nandimandalam varneeth varma <86708658+varneeth01@users.noreply.github.com>
2025-02-21 15:13:58 +0530

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,20 @@
+{pkgs}: {
+  deps = [
+    pkgs.openssh
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.which
+    pkgs.libpng
+    pkgs.libjpeg_turbo
+    pkgs.unzip
+  ];
+}
